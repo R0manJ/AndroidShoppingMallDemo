5073777b912b0ddf219228def2262225564a3f1a
R0manJ <zhur0ngjian@126.com>
2017-07-26 11:38:59 +0800

    Initial commit

---
+++ app/src/main/java/com/rjstudio/androidshoppingmalldemo/bean/ToolbarBean.java
@@ -0,0 +1,8 @@
+package com.rjstudio.androidshoppingmalldemo.bean;
+
+/**
+ * Created by r0man on 2017/7/26.
+ */
+
+public class ToolbarBean {
+}
---
+++ app/src/main/java/com/rjstudio/androidshoppingmalldemo/widget/MyToolBar.java
@@ -0,0 +1,8 @@
+package com.rjstudio.androidshoppingmalldemo.widget;
+
+/**
+ * Created by r0man on 2017/7/26.
+ */
+
+public class MyToolBar {
+}
